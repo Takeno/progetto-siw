3533324ab0f84993bafafab82cef03bae170b9b3
Marco Monteleone <mar.monteleone@gmail.com>
2014-05-26 11:44:18 +0200

    prima bozza della classe Amministratore

---
+++ src/it/uniroma3/admin/Amministratore.java
@@ -0,0 +1,62 @@
+package it.uniroma3.admin;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Amministratore {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(nullable = false)
+	private String nickname;
+	@Column(nullable = false)
+	private String password;
+	
+	public Amministratore(String nickname, String password){
+		this.nickname = nickname;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public boolean checkPassword(String password){
+		return this.password.equals(password);
+	}
+	
+	public boolean equals(Object obj){
+		Amministratore admin = (Amministratore)obj;
+		return this.nickname.equals(admin.getNickname()) &&
+				this.password.equals(admin.getPassword());
+	}
+	
+	public int hashCode(){
+		return (this.nickname+this.password).hashCode();
+	}
+}
