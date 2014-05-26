bd4db44de0de6394e89378795fcb9d52fdb01098
Marco Monteleone <mar.monteleone@gmail.com>
2014-05-26 12:45:08 +0200

    prima bozza della classe cliente

---
+++ src/it/uniroma3/customer/Cliente.java
@@ -0,0 +1,155 @@
+package it.uniroma3.customer;
+
+import it.uniroma3.db.orders.Order;
+
+import java.util.Calendar;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	@Column(nullable = false)
+	private String nome;
+	@Column(nullable = false)
+	private String cognome;
+	@Column(unique = true, nullable = false)
+	private String nickname;
+	@Column(nullable = false)
+	private String password;
+	@Temporal(TemporalType.DATE)
+	private Calendar dateOfBirth;
+	@Temporal(TemporalType.DATE)
+	private Calendar registrationDate;
+	@OneToOne
+	private Indirizzo indirizzo;
+	@OneToMany(mappedBy = "cliente")
+	private List<Ordine> ordini;
+	
+	
+	public Cliente(String nome, String cognome, String nickname,
+			String password, Calendar dateOfBirth, Indirizzo indirizzo) {
+		this.nome = nome;
+		this.cognome = cognome;
+		this.nickname = nickname;
+		this.password = password;
+		this.dateOfBirth = dateOfBirth;
+		this.indirizzo = indirizzo;
+		this.registrationDate = Calendar.getInstance();
+	}
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getCognome() {
+		return cognome;
+	}
+
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+
+	public String getNickname() {
+		return nickname;
+	}
+
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public Calendar getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+
+	public void setDateOfBirth(Calendar dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+
+	public Calendar getRegistrationDate() {
+		return registrationDate;
+	}
+
+
+	public void setRegistrationDate(Calendar registrationDate) {
+		this.registrationDate = registrationDate;
+	}
+
+
+	public Indirizzo getIndirizzo() {
+		return indirizzo;
+	}
+
+
+	public void setIndirizzo(Indirizzo indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+
+
+	public List<Ordine> getOrdini() {
+		return ordini;
+	}
+
+
+	public void setOrdini(List<Ordine> ordini) {
+		this.ordini = ordini;
+	}
+	
+	@Override
+	public boolean equals(Object obj){
+		Cliente cust = (Cliente)obj;
+		return this.nickname.equals(cust.getNickname());
+	}
+	
+	@Override
+	public int hashCode(){
+		return this.indirizzo.hashCode()+this.nickname.hashCode();
+	}
+	
+	
+	
+}
